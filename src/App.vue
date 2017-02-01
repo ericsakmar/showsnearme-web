cb54c313298dbc52282630fd806ccb7062a3be87
Eric Sakmar <eric.sakmar@gmail.com>
2017-02-01 17:59:56 -0500

    working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <ShowList :shows="shows"></ShowList>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import moment from 'moment';
+import ShowList from './components/ShowList'
 
 export default {
   name: 'app',
+
   components: {
-    Hello
+    ShowList
+  },
+
+  data() {
+    return {
+      shows: []
+    };
+  },
+
+  created () {
+    this.fetchShows();
+  },
+
+  methods: {
+    fetchShows () {
+      const now = moment();
+      const since = now.format();
+      const until = now.add(1, 'month').format();
+
+      window.fetch(`http://localhost:3001/events?since=${since}&until=${until}`)
+        .then(res => res.json())
+        .then(shows => this.shows = shows);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ShowList.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li v-for="show in groupByDate">{{show.name}}</li>
+  </ul>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'show-list',
+  props: ['shows'],
+
+  computed: {
+    groupByDate: function() {
+
+      const dates = this.shows.reduce((dates, show) => dates.add(moment(show.start_time).format('YYYY-MM-DD')), new Set());
+
+      const more = Array.from(dates).map(date => ({date:date, shows:[]}));
+      console.log(more);
+
+      return this.shows;
+    }
+  }
+}
+</script>
+
