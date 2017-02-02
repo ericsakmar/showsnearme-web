87c14d869c6b6a69712d214119d22964fd05436e
Eric Sakmar <eric.sakmar@gmail.com>
2017-02-01 21:35:47 -0500

    more

--- src/components/ShowList.vue
+++ src/components/ShowList.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="show-list">
-    <ul>
-      <li v-for="show in groupByDate">{{show.name}}</li>
-    </ul>
+
+    <div v-for="day in groupByDate" class="day">
+      <h1 class="header header--day">{{day.date | date}}</h1>
+
+      <article class="show-summary" v-for="show in day.shows">
+
+        <aside class="show-summary__time">
+          <p>{{show.start_time | time }}</p>
+        </aside>
+
+        <div class="show-summary__info">
+          <h1 class="header">{{show.name}}</h1>
+          <p>{{show.place[0].name}}</p>
+        </div>
+
+      </article>
+    </div>
+
   </div>
 </template>
 
@@ -13,19 +28,76 @@ export default {
   name: 'show-list',
   props: ['shows'],
 
+  filters: {
+
+    date: function(value) {
+      return moment(value).calendar(null, {
+        sameDay: '[Today]',
+        nextDay: '[Tomorrow]',
+        nextWeek: 'dddd',
+        lastDay: '[Yesterday]',
+        lastWeek: '[Last] dddd',
+        sameElse: 'dddd, MMMM Do'
+      });
+    },
+
+    time: function(value) {
+      return moment(value).format('h:mma');
+    }
+  },
+
   computed: {
     groupByDate: function() {
 
       const dates = this.shows.reduce((dates, show) => dates.add(moment(show.start_time).format('YYYY-MM-DD')), new Set());
 
-      const more = Array.from(dates).map(date => ({date:date, shows:[]}));
-      console.log(more);
+      const grouped = Array.from(dates).reduce((acc, date) => {
+        acc[date] = [];
+        return acc;
+      }, {});
 
-      return this.shows;
+      this.shows.forEach(show => {
+        const key = moment(show.start_time).format('YYYY-MM-DD');
+        grouped[key].push(show);
+      });
+
+      const arrayed = Object.keys(grouped).map(k => ({date:k, shows:grouped[k]}));
+
+      arrayed.sort((a, b) => {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      });
+
+      return arrayed;
     }
   }
 }
 </script>
 
 <style>
+  .header--day {
+    background: var(--accent-color);
+    margin-bottom: var(--gutter);
+    padding: var(--gutter-small);
+  }
+
+  .show-summary {
+    display: flex;
+    margin-bottom: var(--gutter);
+  }
+
+  .show-summary__time {
+    background: #ccc;
+    flex: 0 0 5em;
+    text-align: center;
+    padding: var(--gutter-small);
+    margin-right: var(--gutter-small);
+  }
+
+  .show-summary__info {
+    flex-grow: 1;
+    margin-top: var(--gutter-small);
+    margin-bottom: var(--gutter-small);
+  }
+
+
 </style>
